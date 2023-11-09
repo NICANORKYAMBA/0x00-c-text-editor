7612908e8c6463bb1903c95371c3dc1f9a222e5f
NICANORKYAMBA <nicanorkyamba98@gmail.com>
2023-11-09 12:44:35 +0300

    Text editor header file

--- text_editor.h
+++ text_editor.h
@@ -4,36 +4,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 
 /**
- * Line - line of text in the text editor
+ * struct Line - line of text in the text editor
  *
  * @text: text of the line in the text editor
  * @next: next line in the text editor
  */
-typedef struct Line {
+typedef struct Line
+{
 	char *text;
 	struct Line *next;
 } Line;
 
 /**
- * TextContent - text content of the text editor
+ * struct TextContent - text content of the text editor
  *
  * @firstLine: first line in the text editor
  * @lastLine: last line in the text editor
  * @numLines: number of lines in the text editor
  */
-typedef struct TextContent {
+typedef struct TextContent
+{
 	Line *firstLine;
 	Line *lastLine;
 	int numLines;
 } TextContent;
 
 void initializeTextEditor(TextContent *text);
-void destroyTextEditor(TextContent *text);
 void displayText(const TextContent *text);
 void insertText(TextContent *text, int position, const char *line);
+char *customFgets(FILE *stream);
 void deleteText(TextContent *text, int position, int length);
 void saveTextToFile(const TextContent *text, const char *filename);
 void cleanupTextEditor(TextContent *editor);
